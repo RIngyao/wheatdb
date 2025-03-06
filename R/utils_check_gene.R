7469696ce730a3af0ae5e6eb570448698b10e387
Rammani10Biswas <rammanibiswas10@gmail.com>
2025-03-06 18:46:09 +0530

    new update

---
+++ R/utils_check_gene.R
@@ -0,0 +1,20 @@
+#' check_gene
+#'
+#' @description A utils function
+#' @param gene string: gene ID
+#' @param check string: what to check - size or name
+#' @return The return value, if any, from executing the utility.
+#'
+#' @noRd
+check_gene <- function(gene, check){
+  # for(gene in 1:length(list)){
+   if(check == "name"){
+     # check for presence of Traes
+     out <- stringr::str_detect(gene,"TraesCS")
+   }else if (check == "size"){
+     out <- (nchar(gene) == 19)
+   }
+  # }
+
+  return(out)
+}
