e2b9a954849e5c5bcbbfd2e3fac2e2d99bac1371
Rammani10Biswas <rammanibiswas10@gmail.com>
2025-03-07 18:48:18 +0530

    new update

--- tests/testthat/test-utils_check_gene.R
+++ tests/testthat/test-utils_check_gene.R
@@ -1,3 +1,8 @@
 testthat::test_that("multiplication works", {
-  expect_equal(check_gene("TraesCS1A03G0011000", "size"), TRUE)
+  expect_equal(check_gene("TraesCS1A03G0010400", "size"), TRUE)
+})
+
+library(testthat)
+testthat::test_that("multiplication works", {
+  expect_equal(check_gene("TraesCS1A03G0010400", "name"), TRUE)
 })
